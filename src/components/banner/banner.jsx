53a3a2c2aeca11439719abbd478b21a89f4650dd
oumarichard <richard.ouma@student.moringaschool.com>
2022-10-03 15:11:56 +0300

    init commit

---
+++ src/components/banner/banner.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './banner.css';
+import img from '../../image/24832.jpg';
+
+function banner() {
+  return (
+
+    <div className="bannercontrol">
+      <Bgimage />
+
+    </div>
+  );
+}
+
+const Bgimage = () => (
+  <div className="banner-img">
+
+    <img src={img} alt="" />
+    <Bgtext />
+  </div>
+);
+
+const Bgtext = () => (
+  <div className="bg-text">
+    <p className="small-letter">
+      THE BEST MOVIE LIBRARY EVER!
+      <br />
+      <h1 className="big-letter">Best Movies</h1>
+      <br />
+      <p className="last-letter">OF ALL TIME</p>
+    </p>
+  </div>
+);
+
+export default banner;
